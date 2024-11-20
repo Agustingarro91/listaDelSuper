bf00fb14d45c799f417bba4bfc7d62afe1cdf605
Agustin <agusdiazgarro@gmail.com>
2024-11-20 16:40:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import hamburgerMenu from "./menu_hambur.js";
+
+    const d = document;
+
+    d.addEventListener("DOMContentLoaded", e =>{
+        hamburgerMenu(".hamburger", ".panel", ".menu a");
+    })
+
+   
